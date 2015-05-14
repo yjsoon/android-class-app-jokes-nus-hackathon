0bc7a155a2254fcaea385097ec828f4ba431dd3f
YJ Soon <yjsoon@tinkertanker.com>
2015-05-14 13:12:11 +0800

    Finished Ex 3.2 - created a Joke model

---
+++ app/src/main/java/Joke.java
@@ -0,0 +1,29 @@
+/**
+ * Created by yingjie on 14/5/15.
+ */
+public class Joke {
+
+    private String mJoke;
+    private String mPunchline;
+
+    public Joke(String mJoke, String mPunchline) {
+        this.mJoke = mJoke;
+        this.mPunchline = mPunchline;
+    }
+
+    public String getJoke() {
+        return mJoke;
+    }
+
+    public void setJoke(String mJoke) {
+        this.mJoke = mJoke;
+    }
+
+    public String getPunchline() {
+        return mPunchline;
+    }
+
+    public void setPunchline(String mPunchline) {
+        this.mPunchline = mPunchline;
+    }
+}
